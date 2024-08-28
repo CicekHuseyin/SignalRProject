3debdc463b2bffd5b321e95eb88e3705c5e1a239
CicekHuseyin <95969675+CicekHuseyin@users.noreply.github.com>
2024-08-28 19:21:36 +0300

    Add project files.

---
+++ InspimoSignalRApi/Hubs/MyHub.cs
@@ -0,0 +1,20 @@
+﻿using InspimoSignalRApi.Models;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+
+namespace InspimoSignalRApi.Hubs
+{
+    public class MyHub : Hub
+    {
+        private readonly Context _contex;
+
+        public MyHub(Context contex)
+        {
+            _contex = contex;
+        }
+        public static List<string> Names { get; set; }=new List<string>();
+
+        public static int ClientCount { get; set; } = 0;
+        public static int RoomCount { get; set; } = 5;
+    }
+}
