e444d3f2ff1b04038bfd02e5011a055ed9016acc
CicekHuseyin <95969675+CicekHuseyin@users.noreply.github.com>
2024-08-28 20:24:03 +0300

    SignalR ile anlık post işlemleri yapıldı.

--- InspimoSignalRApi/Hubs/MyHub.cs
+++ InspimoSignalRApi/Hubs/MyHub.cs
@@ -1,6 +1,7 @@
 ﻿using InspimoSignalRApi.Models;
 using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace InspimoSignalRApi.Hubs
 {
@@ -16,5 +17,14 @@ namespace InspimoSignalRApi.Hubs
 
         public static int ClientCount { get; set; } = 0;
         public static int RoomCount { get; set; } = 5;
+        public async Task SendName(string name)
+        {
+            Names.Add(name);
+            await Clients.All.SendAsync("ReciveName", name);
+        }
+        public async Task GetNames()
+        {
+            await Clients.All.SendAsync("ReciveNames", Names);
+        }
     }
 }
